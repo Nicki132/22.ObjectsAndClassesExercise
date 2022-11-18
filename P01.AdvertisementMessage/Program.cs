5d85de4d37edd0fd23aa940819cdd5e2ca6f6931
Nikolay Stoyanov <nikitig1987@abv.bg>
2022-11-18 15:58:16 +0100

    Add project files.

---
+++ P01.AdvertisementMessage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01.AdvertisementMessage
+{
+    class Message
+    {
+        public string Phrases { get; set; }
+        public string Events { get; set; }
+        public string Authors { get; set; }
+        public string Cities { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Message> messages = new List<Message>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split('_',
+                    StringSplitOptions.RemoveEmptyEntries)
+                string 
+            }
+        }
+    }
+}
---
+++ P06.VehicleCatalogue/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P06.VehicleCatalogue
+{
+    class Catalogue
+    {
+        public string TypeOfVehicle { get; set; }
+        public string ModelOfVehicle { get; set; }
+        public string ColorOfVehicle { get; set; }
+        public int HorsepowerOfVehicle { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
